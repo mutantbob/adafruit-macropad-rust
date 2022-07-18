1b935a9b4eed0152e0ba25aa5cb43b4c3089c70c
Robert Forsman <git@thoth.purplefrog.com>
2022-07-17 22:50:46 -0400

    try to interface with the sh1106, and fail

---
+++ display-exp1/src/graphics_exp.rs
@@ -0,0 +1,83 @@
+use embedded_graphics::drawable::Drawable;
+use embedded_graphics::fonts::{Font6x8, Text};
+use embedded_graphics::geometry::Size;
+use embedded_graphics::primitives::Line;
+use embedded_graphics::style::{PrimitiveStyle, Styled, TextStyle};
+use sh1106::interface::DisplayInterface;
+use sh1106::mode::GraphicsMode;
+
+struct DTWrapper<
+    'a,
+    E,
+    D: embedded_graphics_core::draw_target::DrawTarget<
+        Color = embedded_graphics_core::pixelcolor::BinaryColor,
+        Error = E,
+    >,
+> {
+    inner: &'a mut D,
+}
+
+impl<
+        'a,
+        E,
+        D: embedded_graphics_core::draw_target::DrawTarget<
+            Color = embedded_graphics_core::pixelcolor::BinaryColor,
+            Error = E,
+        >,
+    > embedded_graphics::DrawTarget<embedded_graphics::pixelcolor::BinaryColor>
+    for DTWrapper<'a, E, D>
+{
+    type Error = E;
+
+    fn draw_pixel(
+        &mut self,
+        item: embedded_graphics::drawable::Pixel<embedded_graphics::pixelcolor::BinaryColor>,
+    ) -> Result<(), Self::Error> {
+        let point: embedded_graphics_core::geometry::Point =
+            embedded_graphics_core::geometry::Point::new(item.0.x, item.0.y);
+        let color = if item.1.is_on() {
+            embedded_graphics_core::pixelcolor::BinaryColor::On
+        } else {
+            embedded_graphics_core::pixelcolor::BinaryColor::Off
+        };
+        let p: embedded_graphics_core::Pixel<embedded_graphics_core::pixelcolor::BinaryColor> =
+            embedded_graphics_core::Pixel(point, color);
+        self.inner.draw_iter(core::iter::once(p))
+    }
+
+    fn size(&self) -> Size {
+        let bounds = self.inner.bounding_box();
+        Size::new(bounds.size.width, bounds.size.height)
+    }
+}
+
+pub fn try_to_draw<DI: DisplayInterface>(disp: &mut GraphicsMode<DI>) {
+    use embedded_graphics::pixelcolor::BinaryColor;
+    use embedded_graphics::prelude::Point;
+    let wrapper = &mut DTWrapper { inner: disp };
+    Text::new("hail Bob!", Point::zero())
+        .into_styled(TextStyle::new(Font6x8, BinaryColor::On))
+        .draw(wrapper)
+        .unwrap();
+
+    Text::new("or die", Point::new(5, 10))
+        .into_styled(TextStyle::new(Font6x8, BinaryColor::Off))
+        .draw(wrapper)
+        .unwrap();
+
+    Styled::new(
+        Line::new(Point::new(5, 5), Point::new(30, 10)),
+        PrimitiveStyle::with_stroke(BinaryColor::On, 2),
+    )
+    .draw(wrapper)
+    .unwrap();
+
+    Styled::new(
+        Line::new(Point::new(5, 10), Point::new(30, 20)),
+        PrimitiveStyle::with_stroke(BinaryColor::Off, 2),
+    )
+    .draw(wrapper)
+    .unwrap();
+
+    let _ = disp.flush();
+}
--- display-exp1/src/main.rs
+++ display-exp1/src/main.rs
@@ -4,31 +4,54 @@
 #![no_std]
 #![no_main]
 
-use adafruit_macropad::hal::pio::PIOExt;
-use adafruit_macropad::hal::Timer;
-use adafruit_macropad::Pins;
-use adafruit_macropad::XOSC_CRYSTAL_FREQ;
-use bsp::entry;
-use defmt::*;
-use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
-use embedded_time::fixed_point::FixedPoint;
-use panic_probe as _;
-
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
 use rp_pico as bsp;
 // use sparkfun_pro_micro_rp2040 as bsp;
 
+use adafruit_macropad::hal::pio::PIOExt;
+use adafruit_macropad::hal::{Spi, Timer};
+use adafruit_macropad::Pins;
+use adafruit_macropad::XOSC_CRYSTAL_FREQ;
+use bsp::entry;
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
     pac,
     sio::Sio,
     watchdog::Watchdog,
 };
-//use embedded_hal::prelude::_embedded_hal_blocking_spi_Write;
-use ws2812_pio::Ws2812;
+use defmt::*;
+use defmt_rtt as _;
+use embedded_graphics_core::draw_target::DrawTarget as DT_;
+use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::spi::MODE_0;
+use embedded_time::fixed_point::FixedPoint;
+use embedded_time::rate::Extensions;
+use panic_probe as _;
+use rp_pico::hal::gpio::Readable;
+use sh1106::interface::{DisplayInterface, SpiInterface};
+use sh1106::mode::GraphicsMode;
+use sh1106::Builder;
 use smart_leds_trait::SmartLedsWrite;
+use ws2812_pio::Ws2812;
+
+mod graphics_exp;
+
+macro_rules! macropad_neopixels {
+    ($pins:expr, $pio:expr, $sm0: expr, $clocks:expr, $timer:expr) => {
+        Ws2812::new(
+            $pins.neopixel.into_mode(),
+            &mut $pio,
+            $sm0,
+            $clocks.peripheral_clock.freq(),
+            $timer.count_down(),
+        )
+    };
+    ($pins: expr, $clocks:expr, $timer:expr, $pac:expr) => {{
+        let (mut pio, sm0, _, _, _) = $pac.PIO0.split(&mut $pac.RESETS);
+        macropad_neopixels!($pins, pio, sm0, $clocks, $timer)
+    }};
+}
 
 #[entry]
 fn main() -> ! {
@@ -52,7 +75,7 @@ fn main() -> ! {
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
 
-    let (mut pio, sm0, _,_,_) = pac.PIO0.split(&mut pac.RESETS);
+    // let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
 
     let sio = Sio::new(pac.SIO);
     let pins = Pins::new(
@@ -64,22 +87,75 @@ fn main() -> ! {
 
     let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
 
+    let _ = pins
+        .mosi
+        .into_mode::<adafruit_macropad::hal::gpio::FunctionSpi>();
+    let _ = pins
+        .miso
+        .into_mode::<adafruit_macropad::hal::gpio::FunctionSpi>();
+
+    let spi1 = Spi::<_, _, 8>::new(pac.SPI1).init(
+        &mut pac.RESETS,
+        125_000_000u32.Hz(),
+        1_000_000u32.Hz(),
+        &MODE_0,
+    );
+
+    //
+
+    let mut disp: GraphicsMode<
+        SpiInterface<
+            Spi<_, _, 8_u8>,
+            adafruit_macropad::hal::gpio::Pin<_, adafruit_macropad::hal::gpio::Output<Readable>>,
+            adafruit_macropad::hal::gpio::Pin<_, adafruit_macropad::hal::gpio::Output<Readable>>,
+        >,
+    > = Builder::new()
+        .connect_spi(spi1, pins.oled_dc.into_mode(), pins.oled_cs.into_mode())
+        .into();
+
+    disp.init().unwrap();
+    disp.flush().unwrap();
+    disp.clear();
+
+    check3(&mut disp);
+    // check2(&mut disp);
+
     let mut led_pin = pins.led.into_push_pull_output();
 
-    let neopixel = pins.neopixel;
+    let mut neopixels = macropad_neopixels!(pins, clocks, timer, pac);
 
-    let mut neopixels = Ws2812::new(
-        neopixel.into_mode(),
-        &mut pio,
-        sm0,
-        clocks.peripheral_clock.freq(),
-        timer.count_down(),
+    //
+    //
+
+    let _ = disp.draw_iter(
+        (0..5)
+            .map(move |y| {
+                (0..10).map(move |x| {
+                    embedded_graphics_core::Pixel(
+                        embedded_graphics_core::prelude::Point::new(x, y),
+                        if 0 == 1 & (x + y) {
+                            embedded_graphics_core::pixelcolor::BinaryColor::On
+                        } else {
+                            embedded_graphics_core::pixelcolor::BinaryColor::Off
+                        },
+                    )
+                })
+            })
+            .flatten(),
     );
+
+    let _ = disp.flush();
+
+    let _ = graphics_exp::try_to_draw(&mut disp);
 
     let mut bright = 255;
     loop {
         let rgb = [(bright, 0, 0), (0, bright, 0), (0, 0, bright)];
-        let cmy = [(0, bright, bright), (bright, 0, bright), (bright, bright, 0)];
+        let cmy = [
+            (0, bright, bright),
+            (bright, 0, bright),
+            (bright, bright, 0),
+        ];
         led_pin.set_high().unwrap();
         let _ = neopixels.write(cmy.iter().copied());
         delay.delay_ms(1500);
@@ -88,5 +164,13 @@ fn main() -> ! {
         delay.delay_ms(1500);
     }
 }
+
+/*pub fn check1<DI>(_x: &SpiInterface<DI, DC, CS>)
+{
+
+}*/
+
+// pub fn check2<D: embedded_graphics::DrawTarget>(_x: &mut D) {}
+pub fn check3<DI: DisplayInterface>(_x: &mut GraphicsMode<DI>) {}
 
 // End of file
